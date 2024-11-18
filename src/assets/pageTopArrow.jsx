0215928a18ae24515f74f56b9e34f93479612f60
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2024-11-19 01:42:50 +0530

    footer added

---
+++ src/assets/pageTopArrow.jsx
@@ -0,0 +1,7 @@
+const pageTopArrow = () => {
+    return (
+        <svg viewBox="0 0 48.3 54.54" xmlns="http://www.w3.org/2000/svg"><path d="M5,26.54L24.15,5l19.15,21.54" fill="none" stroke="#1c1d21" stroke-linecap="round" stroke-linejoin="round" stroke-width="10px"/><path d="M5,49.54l19.15-21.54,19.15,21.54" fill="none" stroke="#1c1d21" stroke-linecap="round" stroke-linejoin="round" stroke-width="10px"/></svg>
+    );
+  };
+  
+  export default pageTopArrow;
