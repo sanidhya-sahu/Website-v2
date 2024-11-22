e41198d1da6a1d4cb572e21babe104bad79f334a
Mohak S <mohaks0408@gmail.com>
2024-11-22 08:34:16 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Proj from "./components/ProjectSection/project";
