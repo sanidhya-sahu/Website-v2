e4395e2c06e5e8bebc8bfe2490e923c2749ac7cd
Mohak S <mohaks0408@gmail.com>
2024-11-22 21:16:33 +0530

    Edited footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,29 +1,49 @@
 import "./footer.css";
-import React from 'react'
+import React from "react";
 // import pageTopArrow from '/pageTopArrow.svg'
 export default function footer() {
   return (
     <div className="Mainfooter">
       <div className="footCont1">
         <h1>AI CLUB</h1>
-        <h5>where humans and machines unite</h5>
-        <img onClick={()=>{window.scrollTo({ top: 0, behavior: "smooth" })}} className="topArrowSvg" src="/PublicAssets/pageTopArrow.svg" alt="" srcset="" />
-        <div onClick={()=>{window.scrollTo({ top: 0, behavior: "smooth" })}} className="topArrowtxt" >Page Top</div>
+        <img
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          className="topArrowSvg"
+          src="/PublicAssets/pageTopArrow.svg"
+          alt=""
+        />
+        <div
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          className="topArrowtxt"
+        >
+          Page Top
+        </div>
       </div>
       <div className="footCont2">
-        <div className="foot2cont1">
-        &copy; 2024 AI Club - VIT Bhopal
-        </div>
+        <div className="foot2cont1">&copy; 2024 AI Club - VIT Bhopal</div>
         <div className="foot2cont2">
-          <a target="_blank" href="https://www.linkedin.com/company/aiclub-vitb/mycompany/">LinkedIn</a>
-          <a target="_blank" href="https://www.instagram.com/aiclub.vitb">Instagram</a>
-          <a target="_blank" href="https://github.com/aiclubvitbhopal">Github</a>
-          <a target="_blank" href="#">Discord</a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/company/aiclub-vitb/mycompany/"
+          >
+            LinkedIn
+          </a>
+          <a target="_blank" href="https://www.instagram.com/aiclub.vitb">
+            Instagram
+          </a>
+          <a target="_blank" href="https://github.com/aiclubvitbhopal">
+            Github
+          </a>
+          <a target="_blank" href="#">
+            Discord
+          </a>
         </div>
-        <div className="foot2cont3">
-        Developed by Web Team ’24 💚
-        </div>
+        <div className="foot2cont3">Developed by Web Team ’24 💚</div>
       </div>
     </div>
-  )
+  );
 }
